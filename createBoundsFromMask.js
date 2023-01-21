60e544194b995b1c4a9379c72d4c3da65fa39d4a
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-21 20:30:31 +0530

    createBoundries function created

---
+++ createBoundsFromMask.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const PNG = require("png-js");
+const IMG_WIDTH = 2160;
+
+PNG.decode("shipmask.png", function (data) {
+  const result = {};
+  for (let i = 0; i < data.length; i++) {
+    const row = Math.floor(i / 4 / IMG_WIDTH);
+    if (data[i] === 0 && data[i + 1] === 255 && data[i + 2] === 0) {
+      if (result[row]) {
+        result[row].push((i / 4) % IMG_WIDTH);
+      } else{
+        result[row] = [(i / 4) % IMG_WIDTH]
+      }
+    }
+  }
+});
